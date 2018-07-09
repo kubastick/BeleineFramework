50e37fd7d049f25fceef61ea7094dd014cd5152e
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-07-09 19:12:14 +0200

    Lets start

--- main.go
+++ main.go
@@ -5,3 +5,16 @@ package main
 type component interface {
 	render() string
 }
+
+type Page struct {
+	components []component
+}
+
+func (p Page) Render() string{
+	/*
+	var result string
+	for _,a := range := p.components {
+
+	}\*/
+	//Later xd
+}
