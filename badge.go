eef448f0308e7c6d6d6359efd36a97b7ad801f2c
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-07-10 13:21:04 +0200

    Badge - work in progress

---
+++ badge.go
@@ -0,0 +1,30 @@
+package beleine
+
+import "fmt"
+
+type Badge struct {
+	id string
+	text string
+	js string
+}
+
+func MakeBadge() Badge {
+	return Badge{id:getGlobalID()}
+}
+
+func (b *Badge) GetID() string {
+	return b.id
+}
+
+func (b *Badge) SetText(text string) {
+	b.text = text
+}
+
+func (b *Badge) render() string {
+	return fmt.Sprintf(`<span class="badge badge-secondary">%s</span>`,b.text)
+}
+
+func (b *Badge) renderJS() string {
+	return b.js
+}
+
