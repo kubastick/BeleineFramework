e9a758cd3c4b3d2db9012af62265dcb1abe6b7fb
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-07-12 16:20:58 +0200

    Badge documentation
    Signed-off-by: Jakub Tomana <kubakikmodziasz@gmail.com>

--- badge.go
+++ badge.go
@@ -26,22 +26,38 @@ type Badge struct {
 	js string
 }
 
+//Creates new Badge struct
 func MakeBadge() Badge {
 	return Badge{id:getGlobalID()}
 }
 
+//Returns ID of the Badge
 func (b *Badge) GetID() string {
 	return b.id
 }
 
+//Sets text of Badge
 func (b *Badge) SetText(text string) {
 	b.text = text
 }
 
+//Enable/Disable more rounded Badge
 func (b *Badge) SetPill(pill bool) {
 	b.pill = pill
 }
 
+//Sets Badge style
+//Badge styles:
+//0-Primary
+//1-Secondary
+//2-Success
+//3-Danger
+//4-Warning
+//5-Info
+//6-Light
+//7-Dark
+//
+//Returns error if value is not in 0-7 range
 func (b *Badge) SetStyle(styleNumber int) error {
 	if styleNumber>7 { if styleNumber<0 {
 		return errors.New("value must be in 0-7 range")
@@ -50,14 +66,17 @@ func (b *Badge) SetStyle(styleNumber int) error {
 	return nil
 }
 
+//Returns JS code as string, that sets Badge text
 func (b *Badge) SetTextJS(text string) string {
 	return fmt.Sprintf(`%s.innerHTML="%s"`,b.id,text)
 }
 
+//Returns JS code as string, that returns Badge text
 func (b *Badge) GetTextJS(text string) string {
 	return fmt.Sprintf(`%s.innerHTML`,b.id)
 }
 
+//Sets JS code function, to be executed after click
 func (b *Badge) SetOnClickListener(listener string) {
 	b.js += fmt.Sprintf("%s.onclick = function(){%s}",b.id,listener)
 }
