0a14926f3394a1e66984750ec69216edce6cedba
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-07-12 17:50:58 +0200

    Post request generation fix
    Signed-off-by: Jakub Tomana <kubakikmodziasz@gmail.com>

--- jsgen.go
+++ jsgen.go
@@ -3,6 +3,6 @@ package beleine
 import "fmt"
 
 func PostRequestJS (URL string,jsData string,onSuccesJS string) string {
-	return fmt.Sprintf(`$.post("%s",%s,function(data){%s};`,URL,jsData,onSuccesJS)
+	return fmt.Sprintf(`$.post("%s",%s,function(data){%s});`,URL,jsData,onSuccesJS)
 }
 
