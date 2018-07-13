92805df368d4c32592f39725f2ce8a273c3bfc56
Mikolaj Radkowski <macbooc48@gmail.com>
2018-07-13 10:37:35 +0200

    docs dropdown

--- dropdown.go
+++ dropdown.go
@@ -9,6 +9,7 @@ type Dropdown struct {
 	name string
 }
 
+//INITIALIZE DROPDOWN
 func (b *Button) DropdownEnabled(enable bool) error {
 	if enable && b.collapse {return errors.New("Collapse is already initialized!")}
 	b.dropdown = enable
@@ -18,12 +19,14 @@ func (b *Button) DropdownEnabled(enable bool) error {
 	return nil
 }
 
+//Adds item to dropdown
 func (b *Button) AddDropdownItem(name string) error {
 	if (!b.dropdown) {return errors.New("Before you add item to dropdown initialize dropdown")}
 	b.ddItems[name] = Dropdown{id:getGlobalID(),name:name}
 	return nil
 }
 
+//Gets id of the item of dropdown
 func (b *Button) GetDropdownItemID(itemName string) string {
 	return b.ddItems[itemName].id
 }
