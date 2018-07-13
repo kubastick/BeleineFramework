14ef17387accb92faf179e0f4c553d1548569a8d
Jakub Tomana <kubakikmodziasz@gmail.com>
2018-07-13 15:21:46 +0200

    Typo fix
    Signed-off-by: Jakub Tomana <kubakikmodziasz@gmail.com>

--- main_test.go
+++ main_test.go
@@ -1,8 +1,8 @@
 package beleine
 
 import (
-	"testing"
 	"fmt"
+	"testing"
 )
 
 func TestPagination(t *testing.T) {
@@ -16,7 +16,9 @@ func TestPagination(t *testing.T) {
 	<li id="%s" class="page-item"><a class="page-link" href="#">1</a></li>
   </ul>
 </nav>
-`,pagination.GetID(),pItem.GetID()) {t.Fail()}
+`, pagination.GetID(), pItem.GetID()) {
+		t.Fail()
+	}
 
 	pItem2 := NewPaginationItem()
 	pItem2.SetText("Next")
@@ -30,7 +32,9 @@ func TestPagination(t *testing.T) {
 	<li id="%s" class="page-item"><a class="page-link" href="#">1</a></li><li id="%s" class="page-item disabled"><a class="page-link" href="#">Next</a></li>
   </ul>
 </nav>
-`,pagination.GetID(),pItem.GetID(),pItem2.GetID()) {t.Fail()}
+`, pagination.GetID(), pItem.GetID(), pItem2.GetID()) {
+		t.Fail()
+	}
 }
 
 func TestCollapse(t *testing.T) {
@@ -50,7 +54,9 @@ func TestCollapse(t *testing.T) {
     ipsum
   </div>
 </div>
-			`,collapse.GetID(),collapse.GetCollapseID(),collapse.GetCollapseID(),collapse.GetCollapseID()) {t.Fail()}
+			`, collapse.GetID(), collapse.GetCollapseID(), collapse.GetCollapseID(), collapse.GetCollapseID()) {
+		t.Fail()
+	}
 }
 
 func TestDropdown(t *testing.T) {
@@ -68,112 +74,152 @@ Animals
 <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
 <a id="%s" class="dropdown-item" href="#">Cat</a>
 </div>
-</div>`,dropdown.GetID(),dropdown.GetDropdownItemID("Cat")) {t.Fail()}
+</div>`, dropdown.GetID(), dropdown.GetDropdownItemID("Cat")) {
+		t.Fail()
+	}
 
-//SOMETIMES CRASHES NO IDEA WHY
+	//SOMETIMES CRASHES NO IDEA WHY
 
-//	dropdown.AddDropdownItem("Dog")
-//	if dropdown.render() != fmt.Sprintf(`<div class="dropdown">
-//<button class="btn btn-danger btn-lg dropdown-toggle" type="button" id="a2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-//Animals
-//</button>
-//<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-//<a id="a3" class="dropdown-item" href="#">Cat</a><a id="a4" class="dropdown-item" href="#">Dog</a>
-//</div>
-//</div>` /*,dropdown.GetID(),dropdown.GetDropdownItemID("Cat"),dropdown.GetDropdownItemID("Dog")*/) {
-//	println("|"+dropdown.render()+"|")
-//	t.Fail()
-//	}
+	//	dropdown.AddDropdownItem("Dog")
+	//	if dropdown.render() != fmt.Sprintf(`<div class="dropdown">
+	//<button class="btn btn-danger btn-lg dropdown-toggle" type="button" id="a2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+	//Animals
+	//</button>
+	//<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+	//<a id="a3" class="dropdown-item" href="#">Cat</a><a id="a4" class="dropdown-item" href="#">Dog</a>
+	//</div>
+	//</div>` /*,dropdown.GetID(),dropdown.GetDropdownItemID("Cat"),dropdown.GetDropdownItemID("Dog")*/) {
+	//	println("|"+dropdown.render()+"|")
+	//	t.Fail()
+	//	}
 }
 
 func TestBadge(t *testing.T) {
 	badge := NewBadge()
 
 	badge.SetText("xCairuuu")
-	if badge.render() != fmt.Sprintf(`<span id="%s" class="badge badge-primary">xCairuuu</span>`,badge.GetID()) {t.Fail()}
+	if badge.render() != fmt.Sprintf(`<span id="%s" class="badge badge-primary">xCairuuu</span>`, badge.GetID()) {
+		t.Fail()
+	}
 	badge.SetText("Bye world")
 	badge.SetStyle(2)
 	badge.SetPill(true)
-	if badge.render() != fmt.Sprintf(`<span id="%s" class="badge badge-pill badge-success">Bye world</span>`,badge.GetID()) {t.Fail()}
+	if badge.render() != fmt.Sprintf(`<span id="%s" class="badge badge-pill badge-success">Bye world</span>`, badge.GetID()) {
+		t.Fail()
+	}
 }
 func TestProgress(t *testing.T) {
-	progress :=NewProgress()
+	progress := NewProgress()
 
-	progress.SetMinMax(0,100)
+	progress.SetMinMax(0, 100)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar   " role="progressbar" style="width: 0%s" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100"></div>
 	</div>
-	`,progress.GetID(), "%") {t.Fail()}
+	`, progress.GetID(), "%") {
+		t.Fail()
+	}
 
 	progress.SetLabels(true)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar   " role="progressbar" style="width: 0%s" aria-valuenow="0" aria-valuemin="0" aria-valuemax="100">0%s</div>
 	</div>
-	`,progress.GetID(), "%", "%") {t.Fail()}
+	`, progress.GetID(), "%", "%") {
+		t.Fail()
+	}
 
 	progress.SetPercent(45)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar   " role="progressbar" style="width: 45%s" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">0%s</div>
 	</div>
-	`,progress.GetID(), "%", "%") {t.Fail()}
+	`, progress.GetID(), "%", "%") {
+		t.Fail()
+	}
 
 	progress.SetProgressType(2)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar  bg-info " role="progressbar" style="width: 45%s" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">0%s</div>
 	</div>
-	`,progress.GetID(), "%", "%") {t.Fail()}
+	`, progress.GetID(), "%", "%") {
+		t.Fail()
+	}
 
 	progress.SetStriped(true)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar progress-bar-striped bg-info " role="progressbar" style="width: 45%s" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">0%s</div>
 	</div>
-	`,progress.GetID(), "%", "%") {t.Fail()}
+	`, progress.GetID(), "%", "%") {
+		t.Fail()
+	}
 
 	progress.SetAnimation(true)
 	if progress.render() != fmt.Sprintf(`
 	<div id="%s" class="progress">
   		<div class="progress-bar progress-bar-striped bg-info progress-bar-animated" role="progressbar" style="width: 45%s" aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">0%s</div>
 	</div>
-	`,progress.GetID(), "%", "%") {t.Fail()}
+	`, progress.GetID(), "%", "%") {
+		t.Fail()
+	}
 }
 
 func TestInput(t *testing.T) {
 	input := NewInput()
 	input.SetHint("Username")
-	if input.render() != fmt.Sprintf(`<input id="%s" type="text" class="form-control " placeholder="Username">`,input.GetID()) {t.Fail()}
+	if input.render() != fmt.Sprintf(`<input id="%s" type="text" class="form-control " placeholder="Username">`, input.GetID()) {
+		t.Fail()
+	}
 	input.SetSize(2)
-	if input.render() != fmt.Sprintf(`<input id="%s" type="text" class="form-control form-control-lg" placeholder="Username">`,input.GetID()) {t.Fail()}
+	if input.render() != fmt.Sprintf(`<input id="%s" type="text" class="form-control form-control-lg" placeholder="Username">`, input.GetID()) {
+		t.Fail()
+	}
 	input.SetInputType("password")
-	if input.render() != fmt.Sprintf(`<input id="%s" type="password" class="form-control form-control-lg" placeholder="Username">`,input.GetID()) {t.Fail()}
+	if input.render() != fmt.Sprintf(`<input id="%s" type="password" class="form-control form-control-lg" placeholder="Username">`, input.GetID()) {
+		t.Fail()
+	}
 }
 
 func TestButton(t *testing.T) {
 	button := NewButton()
 	button.SetText("Click")
-	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-primary ">Click</button>`,button.GetID()) {t.Fail()}
+	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-primary ">Click</button>`, button.GetID()) {
+		t.Fail()
+	}
 	button.SetSize(0)
-	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-primary btn-sm">Click</button>`,button.GetID()) {t.Fail()}
+	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-primary btn-sm">Click</button>`, button.GetID()) {
+		t.Fail()
+	}
 	button.SetButtonType(1)
-	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-secondary btn-sm">Click</button>`,button.GetID()) {t.Fail()}
+	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-secondary btn-sm">Click</button>`, button.GetID()) {
+		t.Fail()
+	}
 	button.SetOutline(true)
-	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-outline-secondary btn-sm">Click</button>`,button.GetID()) {t.Fail()}
+	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-outline-secondary btn-sm">Click</button>`, button.GetID()) {
+		t.Fail()
+	}
 	button.SetState(false)
-	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-outline-secondary btn-sm" disabled>Click</button>`,button.GetID()) {t.Fail()}
+	if button.render() != fmt.Sprintf(`<button id="%s" type="button" class="btn btn-outline-secondary btn-sm" disabled>Click</button>`, button.GetID()) {
+		t.Fail()
+	}
 }
 
 func TestLabel(t *testing.T) {
 	label := NewLabel()
 	label.SetText("Hello world")
-	if label.render() != fmt.Sprintf(`<p id="%s">Hello world</p>`,label.GetID()) {t.Fail()}
+	if label.render() != fmt.Sprintf(`<p id="%s">Hello world</p>`, label.GetID()) {
+		t.Fail()
+	}
 	label.SetSize(1)
-	if label.render() != fmt.Sprintf(`<h1 id="%s">Hello world</h1>`,label.GetID()) {t.Fail()}
+	if label.render() != fmt.Sprintf(`<h1 id="%s">Hello world</h1>`, label.GetID()) {
+		t.Fail()
+	}
 	label.SetSize(6)
-	if label.render() != fmt.Sprintf(`<h6 id="%s">Hello world</h6>`,label.GetID()) {t.Fail()}
+	if label.render() != fmt.Sprintf(`<h6 id="%s">Hello world</h6>`, label.GetID()) {
+		t.Fail()
+	}
 }
 
 func TestAlert(t *testing.T) {
@@ -186,7 +232,9 @@ func TestAlert(t *testing.T) {
 	<div id="%s" class=".alert-success">
   		<strong>Hello world</strong> This is alert description
 	</div>
-	`,alert.GetID()) {t.Fail()}
+	`, alert.GetID()) {
+		t.Fail()
+	}
 
 	alert.SetAlertType(6)
 	alert.SetStrongText("World hello")
@@ -194,10 +242,12 @@ func TestAlert(t *testing.T) {
 	<div id="%s" class=".alert-light">
   		<strong>World hello</strong> This is alert description
 	</div>
-	`,alert.GetID()) {t.Fail()}
+	`, alert.GetID()) {
+		t.Fail()
+	}
 }
 
-func TestCore (t *testing.T) {
+func TestCore(t *testing.T) {
 	var testPage Page
 
 	helloworldLabel := NewLabel()
@@ -217,25 +267,29 @@ func TestCore (t *testing.T) {
 	//println(testPage.Render())
 }
 
-func TestJumbotron(t *testing.T){
+func TestJumbotron(t *testing.T) {
 	jumbotron := NewJumbotron()
 
-	if jumbotron.render() != fmt.Sprintf(`<div id="%s" class="jumbotron"></div>`,jumbotron.GetID()) {t.Fail()}
+	if jumbotron.render() != fmt.Sprintf(`<div id="%s" class="jumbotron"></div>`, jumbotron.GetID()) {
+		t.Fail()
+	}
 
 	label := NewLabel()
 	label.SetText("hello")
 	jumbotron.Attach(&label)
 	jumbotron.SetFluid(true)
 
-	if jumbotron.render() !=  fmt.Sprintf(`<div id="%s" class="jumbotron jumbotron-fluid"><p id="%s">hello</p></div>`,jumbotron.id,label.id) {t.Fail()}
+	if jumbotron.render() != fmt.Sprintf(`<div id="%s" class="jumbotron jumbotron-fluid"><p id="%s">hello</p></div>`, jumbotron.id, label.id) {
+		t.Fail()
+	}
 }
 
-func TestCarousel(t *testing.T){
+func TestCarousel(t *testing.T) {
 	carousel := NewCarousel()
-	i := make([]CarouselItem,3)
-	i[0] = CarouselItem{Title:"hello",ImageSource:"yo1.png",AltText:"Lol"}
-	i[1] = CarouselItem{Title:"world",ImageSource:"yo2.png"}
-	i[2] = CarouselItem{Title:"carousel",ImageSource:"yo3.png"}
+	i := make([]CarouselItem, 3)
+	i[0] = CarouselItem{Title: "hello", ImageSource: "yo1.png", AltText: "Lol"}
+	i[1] = CarouselItem{Title: "world", ImageSource: "yo2.png"}
+	i[2] = CarouselItem{Title: "carousel", ImageSource: "yo3.png"}
 	carousel.SetCarouselItems(&i)
 	carousel.SetIndicatorsEnabled(true)
 	carousel.SetCaptionsEnabled(true)
@@ -319,8 +373,8 @@ func BenchmarkPageLoginPrepared(b *testing.B) {
 	submitButton := NewButton()
 	submitButton.SetText("Login")
 	submitButton.SetOnClickListener(
-		PostRequestJS("127.0.0.1/api", fmt.Sprintf("{l:%s,p:%s",login.GetTextJS(),password.GetTextJS()),"") +
-		submitButton.SetTextJS("Logging in..."))
+		PostRequestJS("127.0.0.1/api", fmt.Sprintf("{l:%s,p:%s", login.GetTextJS(), password.GetTextJS()), "") +
+			submitButton.SetTextJS("Logging in..."))
 
 	testPage.Attach(&helloworldLabel)
 	testPage.Attach(&loginCaption)
@@ -332,25 +386,31 @@ func BenchmarkPageLoginPrepared(b *testing.B) {
 	}
 }
 
-func TestNaw(t *testing.T) {
+func TestNav(t *testing.T) {
 	nav := NewNav()
 	nav.SetStyle(1)
 	nav.SetExpand(true)
 
-	nav.AddItem(&NavItem{Title:"yo",Link:"#"})
+	nav.AddItem(&NavItem{Title: "yo", Link: "#"})
 	if nav.render() != `<ul class="nav nav-tabs nav-fill">
 <li class="nav-item">
 	<a class="nav-link" href="#">yo</a>
 </li>
-</ul>` {t.Fail()}
+</ul>` {
+		t.Fail()
+	}
 	nav.SetExpand(false)
 	nav.SetStyle(0)
 	if nav.render() != `<ul class="nav">
 <li class="nav-item">
 	<a class="nav-link" href="#">yo</a>
 </li>
-</ul>` {t.Fail()}
+</ul>` {
+		t.Fail()
+	}
 
 	err := nav.SetStyle(3)
-	if err == nil {t.Fail()}
+	if err == nil {
+		t.Fail()
+	}
 }
